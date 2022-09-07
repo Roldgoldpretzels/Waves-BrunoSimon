12be20e4e91ce53abf29f309e67f6cf682a87c00
Roldgoldpretzels <100233541+Roldgoldpretzels@users.noreply.github.com>
2022-09-07 08:51:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,137 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import terrainVertexShader from './shaders/terrain/vertex.glsl'
+import terrainFragmentShader from './shaders/terrain/fragment.glsl'
+import { WireframeGeometry } from 'three'
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Sizes
+ */
+
+//Debugger
+// const gui = new Guify({
+//     align: 'right',
+//     theme: 'dark',
+//     barMode:'none'
+// })
+
+
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 1
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Terrain
+ */
+
+const terrain = {}
+
+terrain.texture = {}
+terrain.texture.width = 32
+terrain.texture.height = 128
+terrain.texture.canvas = document.createElement('canvas')
+terrain.texture.canvas.width = terrain.texture.width
+terrain.texture.canvas.height = terrain.texture.height
+terrain.texture.canvas.style.position = 'fixed'
+terrain.texture.canvas.style.top = 0
+terrain.texture.canvas.style.left = 0
+terrain.texture.canvas.style.zIndex = 1
+document.body.append(terrain.texture.canvas)
+terrain.texture.canvas = terrain.texture.canvas.getContext('2d')
+terrain.texture.canvas.fillStyle = 'red'
+terrain.texture.canvas.fillRect(0,0, terrain.texture.width, terrain.texture.height)
+
+
+//Geometry 
+terrain.geometry = new THREE.PlaneGeometry(1, 1, 1000,1000)
+terrain.geometry.rotateX(-Math.PI * 0.5)
+
+//Material
+terrain.material = new THREE.ShaderMaterial({
+    transparent: true,
+    blending: THREE.AdditiveBlending,
+    side:THREE.DoubleSide,
+    vertexShader: terrainVertexShader,
+    fragmentShader: terrainFragmentShader
+})
+
+//Mesh
+terrain.mesh = new THREE.Mesh(terrain.geometry,terrain.material)
+terrain.mesh.scale.set(10,10,10)
+scene.add(terrain.mesh)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true,
+})
+renderer.setClearColor(0x111111,1)
+renderer.outputEncoding = THREE.sRGBEncoding
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+scene
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+let lastElapsedTime = 0
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - lastElapsedTime
+    lastElapsedTime = elapsedTime
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
